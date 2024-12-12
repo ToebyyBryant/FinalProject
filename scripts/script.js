54740a1239d47414511b148d103bc5fdc8383a35
ToebyyBryant <bschulz91@gmail.com>
2024-12-11 22:26:26 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Bruce Schulz
+    File Name: script.js
+    Date: 12.03.24
+*/
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function(){
+        $('.hero').height($(window).height());
+    });
